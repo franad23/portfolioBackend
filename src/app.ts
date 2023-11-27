b993878310fdc493732d656d49642a2f4248f02f
Franco Diaz <dfrancoandres@gmail.com>
2023-11-27 10:57:21 -0300

    se corrige cors y se termina de crear endpoint de dejar mensaje y se prueba con front, todo ok

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,10 @@ import homeMessageRoutes from "./routes/homeMessages.routes"
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(morgan("dev"));
 
 app.use("/api", homeMessageRoutes);
